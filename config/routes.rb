e181a293a0aff7228a6ef5072000ce2f65638686
Sheldon <sbmusicfreak15@msn.com>
2018-07-18 19:36:27 -0700

    connecting to new heroku site

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/abouot'
   get 'simple_pages/contact'
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   get 'simple_pages/index'
+  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
