c6a4b2340c5043b1f3a5bba330dc01ae13374148
Sheldon <sbmusicfreak15@msn.com>
2018-08-05 14:44:06 -0700

    fixed config file error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign up' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :users
   resources :products
   get 'simple_pages/abouot'
