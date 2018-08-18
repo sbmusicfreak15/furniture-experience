d6e4bd9d8afbfc44c552592ed5117ff69f94b3ed
Sheldon <sbmusicfreak15@msn.com>
2018-08-17 22:01:59 -0700

    added comment model and 3 comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,15 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
+  public
+
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
 end
