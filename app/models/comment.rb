1cdda6217bca8bac6b7718902548ce92afa1ea4a
Sheldon <sbmusicfreak15@msn.com>
2018-08-20 22:31:46 -0700

    re-arranged product/show page and made it responsive

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  scope :rating_desc, -> { order(rating: :desc) }
   belongs_to :product
+  scope :rating_desc, -> { order(rating: :desc) }
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,14 +2,12 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 
-  public
-
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
 
   def highest_rating_comment
-    comments.rating_desc.first
+    comments.rating_desc.pluck(:rating).first
   end
 
 end
