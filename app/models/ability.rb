acb03aaecc383255807776be1ea2293abe7439c8
Sheldon <sbmusicfreak15@msn.com>
2018-08-26 14:55:03 -0700

    restricted access of destroy method to admin only

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    cannot :destroy, Comment
+    if user.admin?
+      can :manage, :all
+    end
   end
 end
