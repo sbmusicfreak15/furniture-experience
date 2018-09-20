917581cba6d2c933149e0f88cc26429986ca165a
Sheldon <sbmusicfreak15@msn.com>
2018-09-20 14:13:25 -0700

    added real time updates using ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://furniture-experience.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://furniture-experience.herokuapp.com', 'http://furniture-experience.herokuapp.com']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
   post 'simple_pages/thank_you'
   resources :orders, only: [:index, :show, :create, :destroy]
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
