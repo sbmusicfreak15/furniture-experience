05dd51e51d8df3056cd9b29bb5dd1b1ec9d65052
Sheldon <sbmusicfreak15@msn.com>
2018-09-28 18:49:47 -0700

    added additional products to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wa `rs' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "Lexington Sofa", description: "Midsize sofa with loveseat and ottoman.", image_url: "couch7.jpg", color: "Grey", price: 2799)
+Product.create(name: "Clearwater Sofa", description: "Large sectional with recliner", image_url: "couch8.jpg", color: "Cream", price: 1500)
+Product.create(name: "Hermosa Lounge", description: "Midsize modern sofa with love seat", image_url: "sofa.jpg", color: "Black", price: 2299)
+Product.create(name: "Hermosa Lounge", description: "Midsize modern sofa with love seat", image_url: "sofa.jpg", color: "Black", price: 2299)
